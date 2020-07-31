d05b3d4b9c4ec962071db54b162ba2c9296ebd4a
Nicholas Dorr <nicholasadorr@gmail.com>
2020-07-31 13:32:46 +0000

    update to add another folder

--- minio_helm_addon.sh
+++ minio_helm_addon.sh
@@ -6,19 +6,31 @@
 # create date variable
 d=`date +%Y-%m-%d-%H-%M`
 
-# copy old image list from minio
+# move new container list to tmp folder
+mv ~/closed-env-container-images.txt /tmp/full-list
+
+# copy yesterdays image list from minio
 mc cp minio/helmcharts/closed-env-container-images.txt ~/
 
-# rename old image list
-mv ~/closed-env-container-images.txt ~/old-list
+# rename yesterdays image list
+mv ~/closed-env-container-images.txt ~/yester-list
 
-# append new list created to old list
-cat ~/closedcontainerfiles/closed-env-container-images.txt >> ~/old-list
+# append todays list to yesterdays list
+cat /tmp/full-list >> ~/yester-list
 
-# sort list and rename old back to original
-cat ~/old-list | sort | uniq > ~/closed-env-container-images.txt
+# copy old list from minio temp
+mc mv minio/helmcharts/temp/temp-old.txt ~/
 
-# rename and move old list in mino to old_container_lists bucket
+# append old list to yesterdays list
+cat ~/temp-old.txt >> ~/yester-list
+
+# move new full list to mino temp folder
+mc mv /tmp/full-list minio/helmcharts/temp/temp-old.txt
+
+# sort out new unique list and rename back to original
+cat ~/yester-list | sort | uniq -u > ~/closed-env-container-images.txt
+
+# rename and move yesterdays list in minio to old_container_lists bucket
 mc mv minio/helmcharts/closed-env-container-images.txt minio/helmcharts/old_container_lists/closed-evn-container-images-$d
 
 # move new unique list back to minio
@@ -37,6 +49,6 @@ mc rm --recursive --force --older-than 7d minio/helmcharts/old_charts/
 mc rm --recursive --force --older-than 30d minio/helmcharts/old_container_lists/
 
 # cleanup
-rm -rf ~/helm-charts.tar ~/closed-env-container-images.txt ~/closedcontainerfiles ~/old-list
+rm -rf ~/helm-charts.tar ~/closed-env-container-images.txt ~/temp-old.txt ~/yester-list
 
 echo happy dance
